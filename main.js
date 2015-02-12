82ca449d4ffb142127b2c71942e20fd1ca455980
Jon Wolfe <jonathanwolfe5@gmail.com>
2015-02-12 09:32:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,6 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-/*jslint vars: true, plusplus: true, devel: true, nomen: true,  regexp: true, indent: 4, maxerr: 50 */
 /*global define, brackets */
 
 define(function (require, exports, module) {
