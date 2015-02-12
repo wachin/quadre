100fb4c85a568c81bc09a00e0e490a20a0497d25
Jon Wolfe <jonathanwolfe5@gmail.com>
2015-02-12 09:31:35 -0500

    update regex & add path matching
    This lets us exclude files that are skipped in apparently protected folders like ".git"

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,20 +24,31 @@
 /*global define, brackets */
 
 define(function (require, exports, module) {
-    "use strict";
-    
-    var FileSystem  = brackets.getModule("filesystem/FileSystem");
-    
-    var _oldFilter = FileSystem._FileSystem.prototype._indexFilter;
-    
-    FileSystem._FileSystem.prototype._indexFilter = function (path, name) {
-        // Call old filter
-        var result = _oldFilter.apply(this, arguments);
-        
-        if (!result) {
-            return false;
-        }
-        
-        return !name.match(/node_modules|^.bower-cache$|^.bower-tmp$|^.bower-registry$|^vendor$/);
-    };
+	"use strict";
+
+	var FileSystem = brackets.getModule("filesystem/FileSystem");
+
+	var _oldFilter = FileSystem._FileSystem.prototype._indexFilter;
+
+	var regex = "node_modules|bower_components|/.git/|^vendor$";
+
+	FileSystem._FileSystem.prototype._indexFilter = function (path, name) {
+		// Call old filter
+		var result = _oldFilter.apply(this, arguments);
+
+		if (!result) {
+			return false;
+		}
+
+		var path_matched = path.match(regex);
+		var name_matched = name.match(regex);
+
+//		console.group();
+//		console.log(path, !path_matched);
+//		console.log(name, !name_matched);
+//		console.log('verdict', (!path_matched && !name_matched) ? 'show' : 'hide');
+//		console.groupEnd();
+
+		return !path_matched && !name_matched;
+	};
 });
