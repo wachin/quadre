d04ac79e912f9c9775576ab99459122af1576094
Martin Zagora <zaggino@gmail.com>
2015-05-22 16:47:57 +1000

    use forked version of electron-packager

--- bundle.sh
+++ bundle.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-electron-packager . Bracketron --platform=$1 --arch=$2 --version=$3 --ignore='./node_modules/\.bin$' --ignore='./node_modules/electron-rebuild$' --ignore='./node_modules/grunt[-a-z]*$'
+grunt build
+electron-packager . Bracketron --platform=$1 --arch=$2 --version=$3 --ignore='^node_modules/\.bin$' --ignore='^node_modules/grunt[-a-z]*$'
