01249db18f3b2bae0b7a34add1133c0155edc233
Jon Wolfe <jon@socialtopias.com>
2015-08-18 14:28:23 -0400

    finish new filter making

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
+ * copy of this software and associated documentation files (the 'Software'),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
@@ -11,7 +11,7 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -23,19 +23,22 @@
 /*global define, brackets */
 
 define(function (require, exports, module) {
-	"use strict";
+    'use strict';
 
-	// Get our Brackets modules
-	var ProjectMangager = brackets.getModule("project/ProjectManager");
-	var PreferencesManager = brackets.getModule("preferences/PreferencesManager");
-	var AppInit = brackets.getModule("utils/AppInit");
+    // Get our Brackets modules
+    var FileSystem = brackets.getModule("filesystem/FileSystem")._FileSystem;
+    var ProjectMangager = brackets.getModule('project/ProjectManager');
+    var PreferencesManager = brackets.getModule('preferences/PreferencesManager');
+    var AppInit = brackets.getModule('utils/AppInit');
+    var _oldFilter = FileSystem.prototype._indexFilter;
+    var _newFilter = [];
 
-	// Load up Multimatch
-	var multimatch = require("includes/multimatch");
+    // Load up Multimatch
+    var multimatch = require('includes/multimatch');
 
-	// Preference Stuff
-	var module_id = 'jwolfe.file-tree-exclude',
-		defaults = [
+    // Preference Stuff
+    var module_id = 'jwolfe.file-tree-exclude',
+        defaults = [
                 'node_modules',
                 'bower_components',
                 '.git',
@@ -43,60 +46,60 @@ define(function (require, exports, module) {
                 'vendor'
             ];
 
-	// Get the preferences for this extension
-	var preferences = PreferencesManager.getExtensionPrefs(module_id);
+    // Get the preferences for this extension
+    var preferences = PreferencesManager.getExtensionPrefs(module_id);
 
-	// If there aren't any preferences, assign the default values
-	if (!preferences.get('list')) {
-		preferences.definePreference('list', 'array', defaults);
-		preferences.set('list', preferences.get('list'));
-	}
+    // If there aren't any preferences, assign the default values
+    if (!preferences.get('list')) {
+        preferences.definePreference('list', 'array', defaults);
+        preferences.set('list', preferences.get('list'));
+    }
 
-	// Define the new filter
-	function filter_files(list, file) {
+    // Define the new filter
+    function filter_files(list, file) {
 
-		var projectRoot = ProjectMangager.getProjectRoot();
-		var projectPath = projectRoot.fullPath;
+        var projectRoot = ProjectMangager.getProjectRoot();
+        var projectPath = projectRoot.fullPath;
 
-		// prep show - default to showing the file
-		var show = true;
+        // No exclusions? Quit early.
+        if (!list.length) {
+            return false;
+        }
 
-		// No exclusions? Quit early.
-		if (!list.length) {
-			return show;
-		}
+        // Make path relative to project
+        var relative_path = file.fullPath.replace(projectPath, '');
+        var matched = multimatch(relative_path, list); // A banned result was in the path
 
-		// Make path relative to project
-		var relative_path = file.fullPath.replace(projectPath, '');
-		var matched = multimatch(relative_path, list); // A banned result was in the path
+        if (matched.length) {
+            _newFilter.push(file.fullPath);
+        }
 
-		if (matched.length) {
-			show = false; // we banned it
-		}
+        // Debug info
+        // console.group(relative_path ? relative_path : '[project_root]')
+        // console.log('file', file);
+        // console.log('matched', matched);
+        // console.log('verdict', matched.length ? 'hide' : 'show');
+        // console.groupEnd();
 
-		// Debug info
-		console.group();
-		console.log('projectPath', projectPath);
-		console.log(relative_path ? relative_path : '[project_root]', matched);
-		console.log('show', show ? 'show' : 'hide');
-		console.groupEnd();
+        return true;
+    }
 
-		return show;
-	}
+    function new_filter(path, name) {
+        var not_in_our_filter = _newFilter.indexOf(path) === -1;
+        return not_in_our_filter ? _oldFilter.apply(this, arguments) : not_in_our_filter;
+    }
 
-	// Use the custom filter when Brackets is done loading
-	AppInit.appReady(function () {
-		ProjectMangager.getAllFiles().then(function (files) {
-			var list = preferences.get('list', preferences.CURRENT_PROJECT);
+    // Use the custom filter when Brackets is done loading
+    AppInit.appReady(function () {
+        ProjectMangager.getAllFiles().then(function (files) {
+            var list = preferences.get('list', preferences.CURRENT_PROJECT);
 
-			console.log('list', list);
-			console.log('files', files);
+            files.forEach(function (file) {
+                filter_files(list, file);
+            });
 
-			files.forEach(function (file) {
-				filter_files(list, file);
-			});
-		});
-
-		ProjectMangager.refreshFileTree();
-	});
+            FileSystem.prototype._indexFilter = new_filter;
+            ProjectMangager.refreshFileTree();
+        });
+    });
 });
