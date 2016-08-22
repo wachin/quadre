6baa5b4998e2ceac46b967b414c7bceec8558c40
Martin Zagora <zaggino@gmail.com>
2016-08-22 12:10:49 +1000

    sync package.json to electron-app folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const fs = require('fs');
 const gulp = require('gulp');
 const path = require('path');
 const watch = require('gulp-watch');
@@ -5,6 +7,25 @@ const watch = require('gulp-watch');
 const BASE_DIR = 'electron-app';
 const DIST_DIR = 'dist';
 const JS_GLOB = `${BASE_DIR}/**/*.{js,json}`;
+
+function syncPackageJson() {
+    const packageJson = require(path.resolve(__dirname, 'package.json'));
+    const appJson = _.pick(packageJson, [
+        'name',
+        'productName',
+        'description',
+        'author',
+        'license',
+        'homepage',
+        'version',
+        'apiVersion',
+        'issues',
+        'repository',
+        'dependencies',
+        'optionalDependencies'
+    ]);
+    fs.writeFileSync(path.resolve(__dirname, BASE_DIR, 'package.json'), JSON.stringify(appJson, null, 2));
+}
 
 function copyJs(filePath) {
     let from;
@@ -21,7 +42,10 @@ function copyJs(filePath) {
         .pipe(gulp.dest(to));
 }
 
-gulp.task('build', () => copyJs());
+gulp.task('build', () => {
+    syncPackageJson();
+    copyJs();
+});
 
 gulp.task('watch', ['build'], () => {
     watch(JS_GLOB, file => copyJs(file.path));
