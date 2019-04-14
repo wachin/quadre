cd54bdc3caef6f199968b8644b72ba9d6e7bc66b
ficristo <ficristo.work@gmail.com>
2019-04-14 16:08:46 +0200

    Update some deps (#224)

--- src/editor/EditorManager.ts
+++ src/editor/EditorManager.ts
@@ -547,8 +547,8 @@ function _showEditor(document, pane, editorOptions) {
     if (!editor) {
         // Performance (see #4757) Chrome wastes time messing with selection
         // that will just be changed at end, so clear it for now
-        if (window.getSelection && window.getSelection().empty) {  // Chrome
-            window.getSelection().empty();
+        if (window.getSelection && window.getSelection()!.empty) {  // Chrome
+            window.getSelection()!.empty();
         }
 
         // Editor doesn't exist: populate a new Editor with the text
